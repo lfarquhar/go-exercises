4250d7023c601c8005f613fd4ba67c7b8c09cd8b
lfarquhar <lance.farquhar@gmail.com>
2024-11-25 08:54:33 -0500

    Generics

---
+++ Generics/2.customConstraints/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+func chargeForLineItem[T lineItem](newItem T, oldItems []T, balance float64) ([]T, float64, error) {
+	newBalance := balance - newItem.GetCost()
+
+	if newBalance < 0 {
+		err := errors.New("insufficient funds")
+		return nil, 0.00, err
+	}
+
+	oldItems = append(oldItems, newItem)
+
+	return oldItems, newBalance, nil
+}
+
+/*
+Complete the chargeForLineItem function. First, it should check if the user has a balance with enough funds
+to be able to pay for the cost of the newItem. If they don't, then return an "insufficient funds" error and
+zero values for the other return values.
+
+If they do have enough funds:
+
+Add the line item to the user's history by appending the newItem to the slice of oldItems. This new slice is
+your first return value.
+Calculate the user's new balance by subtracting the cost of the new item from their balance. This is your second return value.
+*/
+
+// don't edit below this line
+
+type lineItem interface {
+	GetCost() float64
+	GetName() string
+}
+
+type subscription struct {
+	userEmail string
+	startDate time.Time
+	interval  string
+}
+
+func (s subscription) GetName() string {
+	return fmt.Sprintf("%s subscription", s.interval)
+}
+
+func (s subscription) GetCost() float64 {
+	if s.interval == "monthly" {
+		return 25.00
+	}
+	if s.interval == "yearly" {
+		return 250.00
+	}
+	return 0.0
+}
+
+type oneTimeUsagePlan struct {
+	userEmail        string
+	numEmailsAllowed int
+}
+
+func (otup oneTimeUsagePlan) GetName() string {
+	return fmt.Sprintf("one time usage plan with %v emails", otup.numEmailsAllowed)
+}
+
+func (otup oneTimeUsagePlan) GetCost() float64 {
+	const costPerEmail = 0.03
+	return float64(otup.numEmailsAllowed) * costPerEmail
+}
